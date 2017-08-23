3d3b090eaab928efd937bf197265cb2924f5b9e0
chenc <cheng.chen@cern.ch>
2017-08-23 15:39:09 +0200

    setup and install

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ source ve/bin/activate
 
 export PATH="${PWD}:${PATH}"
 export PATH="${PWD}/scripts:${PATH}"
+
 export PYTHONPATH="${PWD}:${PYTHONPATH}"
 export PYTHONPATH="${PWD}/scripts:${PYTHONPATH}"
+export PYTHONPATH="${PWD}/xgboost/python-package":${PYTHONPATH}
+
 export THEANO_FLAGS="gcc.cxxflags='-march=core2'"
